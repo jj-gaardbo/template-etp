130937b78e588a3281df6934d4cbdc7b517a064c
Jens Jákup Egholm Gaardbo <jjg@html24.net>
2018-10-02 21:24:23 +0200

    Added slider + bootstrap

--- header.php
+++ header.php
@@ -9,6 +9,9 @@
 
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro|Source+Serif+Pro" rel="stylesheet">
 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
@@ -32,19 +35,19 @@
 			<!-- header -->
 			<header class="header clear" role="banner">
 
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
+                <!-- nav -->
+                <nav class="nav" role="navigation">
+                    <?php custom_nav(); ?>
+                </nav>
+                <!-- /nav -->
 
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php custom_nav(); ?>
-					</nav>
-					<!-- /nav -->
+                <!-- logo -->
+                <div class="logo">
+                    <a href="<?php echo home_url(); ?>">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
+                    </a>
+                </div>
+                <!-- /logo -->
 
 			</header>
 			<!-- /header -->
