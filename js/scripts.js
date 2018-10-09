c22cd314e078e262dd3327893fdfb0d405475839
Jens Jákup Egholm Gaardbo <jjg@html24.net>
2018-10-09 22:21:47 +0200

    Added video + modal

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,24 @@
         init_searchform();
 
         init_slider();
+
+        init_video_lightbox();
 		
 	});
+
+    function init_video_lightbox(){
+        $(".modal-trigger").click(function(e){
+            e.preventDefault();
+            dataModal = $(this).attr("data-modal");
+            $("#" + dataModal).css({"display":"block"});
+            // $("body").css({"overflow-y": "hidden"}); //Prevent double scrollbar.
+        });
+
+        $(".close-modal, .modal-sandbox").click(function(){
+            $(".modal").css({"display":"none"});
+            // $("body").css({"overflow-y": "auto"}); //Prevent double scrollbar.
+        });
+    }
 
     function init_searchform(){
         var trigger = $('.search-trigger');
--- sections/testimonials.php
+++ sections/testimonials.php
@@ -1,6 +1,8 @@
 <?php
 $testimonial = get_field('fp_quote');
-if(trim($testimonial) == ''){return;}
+$videoLink = get_field('video_link');
+if(trim($testimonial) == '' && $videoLink != ''){return;}
+
 ?>
 <section class="testimonial-section row">
 
@@ -8,17 +10,42 @@ if(trim($testimonial) == ''){return;}
 
         <div class="row">
 
-            <div class="<?php echo get_full_width_classes();?> testimonial">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 testimonial">
 
-                <blockquote>
+                <blockquote class="clearfix">
                     <i class="fas fa-quote-left"></i>
                     <?php echo $testimonial;?>
                 </blockquote>
 
             </div>
 
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 presentation">
+
+                <i class="fas fa-play-circle modal-trigger" data-modal="video-presentation-modal"></i>
+
+            </div>
+
         </div>
 
+    </div>
+
+    <!-- Modal -->
+    <div class="modal video-lightbox" id="video-presentation-modal">
+        <div class="modal-sandbox"></div>
+        <div class="modal-box">
+            <div class="modal-header">
+                <span>ETP Consult - Præsentation</span>
+                <i class="fas fa-times close-modal"></i>
+            </div>
+            <div class="modal-body">
+
+                <?php echo wp_oembed_get( $videoLink );?>
+
+                <?php echo wpautop(get_field('video_description'));?>
+
+                <button class="btn-theme btn-theme-dark-blue close-modal"><?php _e('Close', 'etp-consult')?></button>
+            </div>
+        </div>
     </div>
 
 </section>
