c5907b0f395db98e7e42f0ae37378d8a6594e773
Jens Jákup Egholm Gaardbo <jjg@html24.net>
2018-10-13 11:31:25 +0200

    Small color changes

--- template-frontpage.php
+++ template-frontpage.php
@@ -5,9 +5,9 @@ get_header();
 
     <?php require_once get_template_directory() . '/sections/slider.php';?>
 
-    <?php require_once get_template_directory() . '/sections/cases.php';?>
-
     <?php require_once get_template_directory() . '/sections/testimonials.php';?>
+
+    <?php require_once get_template_directory() . '/sections/cases.php';?>
 
     <?php require_once get_template_directory() . '/sections/news.php';?>
 
