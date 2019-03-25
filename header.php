a84752d4e65f0f59e7e28d494403bb9fd0e4fe12
Jens Jákup Egholm Gaardbo <jjg@html24.net>
2019-03-25 19:01:10 +0100

    Small style fix footer mobile

--- header.php
+++ header.php
@@ -7,9 +7,9 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico?v=2" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+
 
 		<?php wp_head(); ?>
 		<script>
